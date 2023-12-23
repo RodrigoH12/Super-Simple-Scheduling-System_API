d7606314001f3415531e0aa2aac142bc8ce37b45
Rodrigo Heredia <rodrigoh1205@gmail.com>
2023-12-22 22:42:56 -0400

    [Task-3] Created DBContext for the Data layer

---
+++ SuperSimpleSchedulingSystem/Data/DBContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperSimpleSchedulingSystem.Configuration;
+using SuperSimpleSchedulingSystem.Data.Models;
+
+namespace SuperSimpleSchedulingSystem.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(IApplicationConfiguration applicationConfiguration)
+        {
+            _applicationConfiguration = applicationConfiguration;
+        }
+
+        private readonly IApplicationConfiguration _applicationConfiguration;
+        public DbSet<User> User { get; set; }
+        public DbSet<Student> Student { get; set; }
+        public DbSet<Class> Class { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_applicationConfiguration.GetDatabaseConnectionString().DATABASE);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}

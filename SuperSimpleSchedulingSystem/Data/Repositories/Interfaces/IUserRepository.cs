31a59f5d05645f7e69ace5c2a4b6979dfea0b034
Rodrigo Heredia <rodrigoh1205@gmail.com>
2023-12-22 22:43:36 -0400

    [Task-3] Implemented Repositories for all models

---
+++ SuperSimpleSchedulingSystem/Data/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using SuperSimpleSchedulingSystem.Data.Models;
+using SuperSimpleSchedulingSystem.Data.Repositories.Base;
+
+namespace SuperSimpleSchedulingSystem.Data.Repositories.Interfaces
+{
+    public interface IUserRepository : IRepository<User>
+    {
+
+    }
+}

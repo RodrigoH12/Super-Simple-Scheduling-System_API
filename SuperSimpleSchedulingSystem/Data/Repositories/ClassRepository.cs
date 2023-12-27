9fc4ad78478c25b3ea0ac0f1c250f372c3e741e4
Rodrigo Heredia <rodrigoh1205@gmail.com>
2023-12-26 22:38:14 -0400

    [Task-11] Created new GetClassByIdIncludingStudents function in ClassRepository

--- SuperSimpleSchedulingSystem/Data/Repositories/ClassRepository.cs
+++ SuperSimpleSchedulingSystem/Data/Repositories/ClassRepository.cs
@@ -1,4 +1,5 @@
-﻿using SuperSimpleSchedulingSystem.Data.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using SuperSimpleSchedulingSystem.Data.Models;
 using SuperSimpleSchedulingSystem.Data.Repositories.Base;
 using SuperSimpleSchedulingSystem.Data.Repositories.Interfaces;
 
@@ -7,5 +8,12 @@ namespace SuperSimpleSchedulingSystem.Data.Repositories
     public class ClassRepository : Repository<Class>, IClassRepository
     {
         public ClassRepository(DBContext dbContext) : base(dbContext) { }
+
+        public async Task<Class> GetClassByIdIncludingStudents(Guid id)
+        {
+            return await _dbContext.Set<Class>()
+                .Include(c => c.Students)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
     }
 }
